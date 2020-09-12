a82987a9c27646774202860e289a369afe0dabbf
Hanriel <dfnil@live.ru>
2020-09-12 22:36:46 +0500

    CleanUp

--- src/main/java/com/hanriel/unnecessary_additions/UnnecessaryAdditions.java
+++ src/main/java/com/hanriel/unnecessary_additions/UnnecessaryAdditions.java
@@ -1,27 +1,11 @@
 package com.hanriel.unnecessary_additions;
 
-import com.google.gson.JsonObject;
 import com.hanriel.unnecessary_additions.inits.Configs;
 import com.hanriel.unnecessary_additions.inits.InitClient;
 import com.hanriel.unnecessary_additions.inits.InitCommon;
 import com.hanriel.unnecessary_additions.inits.UAContent;
 import net.minecraft.data.DataGenerator;
-import net.minecraft.enchantment.Enchantment;
-import net.minecraft.enchantment.EnchantmentHelper;
-import net.minecraft.enchantment.Enchantments;
-import net.minecraft.entity.monster.EndermanEntity;
-import net.minecraft.item.ItemStack;
-import net.minecraft.loot.LootContext;
-import net.minecraft.loot.LootParameterSets;
-import net.minecraft.loot.LootParameters;
-import net.minecraft.loot.LootTable;
-import net.minecraft.loot.conditions.ILootCondition;
-import net.minecraft.util.ResourceLocation;
 import net.minecraftforge.common.MinecraftForge;
-import net.minecraftforge.common.loot.GlobalLootModifierSerializer;
-import net.minecraftforge.common.loot.LootModifier;
-import net.minecraftforge.event.LootTableLoadEvent;
-import net.minecraftforge.event.RegistryEvent;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
@@ -30,9 +14,6 @@ import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import net.minecraftforge.fml.loading.FMLPaths;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-
-import javax.annotation.Nonnull;
-import java.util.List;
 
 @Mod(UnnecessaryAdditions.MODID)
 public class UnnecessaryAdditions {
@@ -54,13 +35,14 @@ public class UnnecessaryAdditions {
         modEventBus.addListener(this::gatherData);
     }
 
+    @SubscribeEvent
     private void gatherData(final GatherDataEvent event) {
         final DataGenerator gen = event.getGenerator();
 
-        //if (event.includeServer()) {
+        if (event.includeServer()) {
             //gen.addProvider(new TrapcraftRecipeProvider(gen));
             gen.addProvider(new UALootTableProvider(gen));
-        //}
+        }
     }
 
 }
